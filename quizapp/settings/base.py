1c438eb9dd4c1fe48003d46d6dc96aca8986a505
neerajadhav <adhavneeraj9500@gmail.com>
2025-10-03 09:19:35 +0530

    bootstrap

--- quizapp/settings/base.py
+++ quizapp/settings/base.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "modelcluster",
     "taggit",
     "django_filters",
+    "widget_tweaks",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
